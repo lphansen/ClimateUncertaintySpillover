4e65eb918dc624d09e987b0b3d1c65db9b1c96e1
SuriChen1028 <shirui.chan@gmail.com>
2021-01-21 01:42:13 -0600

    add compute_h_hat func

--- source/utilities.py
+++ source/utilities.py
@@ -14,3 +14,18 @@ def find_nearest_value(array, value):
     """
     loc = np.abs(array - value).argmin()
     return loc
+
+def compute_h_hat(e, gamma_e, xi_e):
+    """
+    compute h hat
+    
+    """
+    sigma_n = 1.2
+    median = 1.75/1000
+    eta = .032
+    gamma = gamma_e
+    xi = xi_e
+    h_hat = e*median*gamma*sigma_n/xi
+    h_hat = h_hat*median*1000*sigma_n/(1-eta)
+    
+    return h_hat
