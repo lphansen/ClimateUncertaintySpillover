ccc04649879c9d1e7fc663212d03be266be6eff6
hanxuh-hub <hanxuh@uchicago.edu>
2021-02-01 19:14:50 +0800

    added cpp notebook

--- source/utilities.py
+++ source/utilities.py
@@ -6,11 +6,54 @@ import numpy as np
 from numba import njit
 
 
-@njit
-def find_nearest_value(array, value):
-    """
-    Find nearest value for 
-    
-    """
-    loc = np.abs(array - value).argmin()
-    return loc
+def compute_derivatives(data, dim, order, dlt):  
+    # compute the central difference derivatives for given input and dimensions
+    res = np.zeros(data.shape)
+    l = len(data.shape)
+    if l == 3:
+        if order == 1:                    # first order derivatives
+            if dim == 0:                  # to first dimension
+                res[1:-1,:,:] = (1 / (2 * dlt)) * (data[2:,:,:] - data[:-2,:,:])
+                res[-1,:,:] = (1 / dlt) * (data[-1,:,:] - data[-2,:,:])
+                res[0,:,:] = (1 / dlt) * (data[1,:,:] - data[0,:,:])
+            elif dim == 1:                # to second dimension
+                res[:,1:-1,:] = (1 / (2 * dlt)) * (data[:,2:,:] - data[:,:-2,:])
+                res[:,-1,:] = (1 / dlt) * (data[:,-1,:] - data[:,-2,:])
+                res[:,0,:] = (1 / dlt) * (data[:,1,:] - data[:,0,:])
+            elif dim == 2:                # to third dimension
+                res[:,:,1:-1] = (1 / (2 * dlt)) * (data[:,:,2:] - data[:,:,:-2])
+                res[:,:,-1] = (1 / dlt) * (data[:,:,-1] - data[:,:,-2])
+                res[:,:,0] = (1 / dlt) * (data[:,:,1] - data[:,:,0])
+        elif order == 2:
+            if dim == 0:                  # to first dimension
+                res[1:-1,:,:] = (1 / dlt ** 2) * (data[2:,:,:] + data[:-2,:,:] - 2 * data[1:-1,:,:])
+                res[-1,:,:] = (1 / dlt ** 2) * (data[-1,:,:] + data[-3,:,:] - 2 * data[-2,:,:])
+                res[0,:,:] = (1 / dlt ** 2) * (data[2,:,:] + data[0,:,:] - 2 * data[1,:,:])
+            elif dim == 1:                # to second dimension
+                res[:,1:-1,:] = (1 / dlt ** 2) * (data[:,2:,:] + data[:,:-2,:] - 2 * data[:,1:-1,:])
+                res[:,-1,:] = (1 / dlt ** 2) * (data[:,-1,:] + data[:,-3,:] - 2 * data[:,-2,:])
+                res[:,0,:] = (1 / dlt ** 2) * (data[:,2,:] + data[:,0,:] - 2 * data[:,1,:])
+            elif dim == 2:                # to third dimension
+                res[:,:,1:-1] = (1 / dlt ** 2) * (data[:,:,2:] + data[:,:,:-2] - 2 * data[:,:,1:-1])
+                res[:,:,-1] = (1 / dlt ** 2) * (data[:,:,-1] + data[:,:,-3] - 2 * data[:,:,-2])
+                res[:,:,0] = (1 / dlt ** 2) * (data[:,:,2] + data[:,:,0] - 2 * data[:,:,1])
+    elif l == 2:
+        if order == 1:                    # first order derivatives
+            if dim == 0:                  # to first dimension
+                res[1:-1,:] = (1 / (2 * dlt)) * (data[2:,:] - data[:-2,:])
+                res[-1,:] = (1 / dlt) * (data[-1,:] - data[-2,:])
+                res[0,:] = (1 / dlt) * (data[1,:] - data[0,:])
+            elif dim == 1:                # to second dimension
+                res[:,1:-1] = (1 / (2 * dlt)) * (data[:,2:] - data[:,:-2])
+                res[:,-1] = (1 / dlt) * (data[:,-1] - data[:,-2])
+                res[:,0] = (1 / dlt) * (data[:,1] - data[:,0])        
+        elif order == 2:
+            if dim == 0:                  # to first dimension
+                res[1:-1,:] = (1 / dlt ** 2) * (data[2:,:] + data[:-2,:] - 2 * data[1:-1,:])
+                res[-1,:] = (1 / dlt ** 2) * (data[-1,:] + data[-3,:] - 2 * data[-2,:])
+                res[0,:] = (1 / dlt ** 2) * (data[2,:] + data[0,:] - 2 * data[1,:])
+            elif dim == 1:                # to second dimension
+                res[:,1:-1] = (1 / dlt ** 2) * (data[:,2:] + data[:,:-2] - 2 * data[:,1:-1])
+                res[:,-1] = (1 / dlt ** 2) * (data[:,-1] + data[:,-3] - 2 * data[:,-2])
+                res[:,0] = (1 / dlt ** 2) * (data[:,2] + data[:,0] - 2 * data[:,1])
+    return res
