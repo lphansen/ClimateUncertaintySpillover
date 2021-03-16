0389b523c2e6e31a79f7fd47499bde87608a0410
hanxu <hanxuh@uchicago.edu>
2021-03-16 20:20:03 +0800

    added linear ode solver

--- source/model.py
+++ source/model.py
@@ -368,3 +368,91 @@ def ode_y_jump_approach_two(y_grid, model_paras=(), v0=None, ϵ=.5, tol=1e-8, ma
            'd_Λ': d_Λ}
 
     return res
+
+
+def ode_y_jump_approach_one_boundary(y_grid, model_paras=(), v0=None, ϵ=.5, tol=1e-8, max_iter=10_000, print_all=True):
+    η, δ, θ, πc_o, σ_y, ξ_1m, ξ_2m, ξ_a, ς, γ_1, γ_2, y_bar, ϕ_i, πd_o = model_paras
+    Δ_y = y_grid[1] - y_grid[0]
+
+    if v0 is None:
+        v0 = -δ*η*(y_grid+y_grid**2)
+
+    d_Λ = γ_1 + γ_2*y_grid
+    dd_Λ = γ_2
+
+    πd = np.ones((len(πd_o), len(y_grid)))
+    for i in range(πd.shape[0]):
+        πd[i] = πd_o[i]
+    πd_o = πd
+
+    πc = np.ones((len(πc_o), len(y_grid)))
+    θ_reshape = np.ones_like(πc)
+    for i in range(πc.shape[0]):
+        πc[i] = πc_o[i]
+        θ_reshape[i] = θ[i]
+    πc_o = πc.copy()
+    θ = θ_reshape
+    
+    e_tilde = 0.
+    
+    count = 0
+    error = 1.
+
+    while error > tol and count < max_iter:
+        v_old = v0.copy()
+        e_tilde_old = e_tilde
+
+        v0_dy = compute_derivatives(v0, 1, Δ_y, central_diff=False)
+        v0_dyy = compute_derivatives(v0, 2, Δ_y)
+
+        G = v0_dy + (η-1)*d_Λ
+
+        if σ_y == 0:
+            e_tilde = -δ*η/(G*np.sum(πc*θ, axis=0))
+        else:
+            temp = σ_y**2*(v0_dyy+(η-1.)*dd_Λ-G**2/ξ_1m)
+            root = np.sum(πc*θ, axis=0)**2*G**2 - 4*δ*η*temp
+            root[root<0] = 0.
+            e_tilde = (-G*np.sum(πc*θ, axis=0) - np.sqrt(root)) / (2*temp)
+        
+        e_tilde[e_tilde<=0] = 1e-16
+        e_tilde = e_tilde *.5 + e_tilde_old*.5
+
+        log_πc_ratio = -G*e_tilde*θ/ξ_a
+        πc_ratio = log_πc_ratio - np.max(log_πc_ratio, axis=0)
+        πc = np.exp(πc_ratio) * πc_o
+        πc = πc/np.sum(πc, axis=0)
+        πc[πc<=0] = 1e-16        
+        c_entropy = np.sum(πc*(np.log(πc)-np.log(πc_o)), axis=0)
+
+        intensity = 1./np.sqrt(ς)*np.exp(-(y_bar-y_grid)**2/(2*ς**2))
+        g = np.exp(1./ξ_2m*(v0-ϕ_i))
+
+        A = np.ones_like(y_grid)*(-δ)
+        B = e_tilde * np.sum(πc*θ, axis=0)
+        C = .5 * σ_y**2 * e_tilde**2
+        D = δ*η*np.log(e_tilde) - C*G**2/ξ_1m + (η-1)*d_Λ*e_tilde*np.sum(πc*θ, axis=0)\
+            + .5*(η-1)*dd_Λ*σ_y**2*e_tilde**2 + ξ_a*c_entropy\
+
+        bc = -ξ_2m*np.log(np.sum(πd_o[:, -1]*np.exp(-1./ξ_2m*ϕ_i[:, -1])))
+
+        v0 = false_transient_one_iteration_python(A, B, C, D, v0, ε, Δ_y, (0, bc), (False, True))
+
+        rhs_error = A*v0 + B*v0_dy + C*v0_dyy + D
+        rhs_error = np.max(abs(rhs_error))
+        lhs_error = np.max(abs((v0 - v_old)/ϵ))
+        error = lhs_error
+        count += 1
+        if print_all:
+            print("Iteration %s: LHS Error: %s; RHS Error %s" % (count, lhs_error, rhs_error))
+
+    print("Converged. Total iteration %s: LHS Error: %s; RHS Error %s" % (count, lhs_error, rhs_error))     
+    res = {'v0': v0,
+           'v0_dy': v0_dy,
+           'v0_dyy': v0_dyy,
+           'e_tilde': e_tilde,
+           'πc': πc,
+           'g': g,
+           'c_entropy': c_entropy,
+           'd_Λ': d_Λ}
+    return res
--- source/solver.py
+++ source/solver.py
@@ -9,7 +9,7 @@ from numba import njit
 
 
 @njit
-def upwind(LHS, A, B, C, i, dx, ϵ):
+def upwind(LHS, A, B, C, i, dx, ϵ, linear_ode=False):
     """
     Compute the coefficient of the equation at v(i).
 
@@ -30,7 +30,10 @@ def upwind(LHS, A, B, C, i, dx, ϵ):
 
     """
     I = len(A)
-    LHS[i, i] += A[i] - 1./ϵ
+    if linear_ode:
+        LHS[i, i] += A[i]
+    else:
+        LHS[i, i] += A[i] - 1./ϵ
     if i == 0:
         LHS[i, i] += B[i] * (-1./dx) + C[i] * (1./dx**2)
         LHS[i, i+1] += B[i] * (1./dx) + C[i] * (-2./dx**2)
@@ -48,7 +51,7 @@ def upwind(LHS, A, B, C, i, dx, ϵ):
 
 
 @njit
-def construct_matrix(A, B, C, D, v0, ϵ, dx, bc, impose_bc):
+def construct_matrix(A, B, C, D, v0, ϵ, dx, bc, impose_bc, linear_ode=False):
     """
     Construct coefficient matrix of the linear system.
     
@@ -75,7 +78,10 @@ def construct_matrix(A, B, C, D, v0, ϵ, dx, bc, impose_bc):
     """
     I = len(A)
     LHS = np.zeros((I, I))
-    RHS = - D - 1./ϵ*v0
+    if linear_ode:
+        RHS = - D
+    else:
+        RHS = - D - 1./ϵ*v0
     for i in range(I):
         if i == 0 and impose_bc[0]:
             LHS[i, i] = 1.
@@ -84,11 +90,17 @@ def construct_matrix(A, B, C, D, v0, ϵ, dx, bc, impose_bc):
             LHS[i, i] = 1.
             RHS[i] = bc[1]
         else:
-            LHS = upwind(LHS, A, B, C, i, dx, ϵ)
+            LHS = upwind(LHS, A, B, C, i, dx, ϵ, linear_ode)
     return LHS, RHS
 
 
 def false_transient_one_iteration_python(A, B, C, D, v0, ϵ, dx, bc, impose_bc):
     LHS, RHS = construct_matrix(A, B, C, D, v0, ϵ, dx, bc, impose_bc)
+    v, exit_code = bicg(csc_matrix(LHS), RHS)
+    return v
+
+
+def solve_lienar_ode_python(A, B, C, D, v0, dx, bc, impose_bc):
+    LHS, RHS = construct_matrix(A, B, C, D, v0, 1., dx, bc, impose_bc, linear_ode=True)
     v, exit_code = bicg(csc_matrix(LHS), RHS)
     return v
