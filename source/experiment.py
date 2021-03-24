bd948d64ff6057295cc719e3416f12794812c785
hanxu <hanxuh@uchicago.edu>
2021-03-25 02:47:02 +0800

    updated codes using scaled HJB

--- source/experiment.py
+++ source/experiment.py
@@ -28,7 +28,7 @@ def solve_value_function(ξ_w, ξ_p, ξ_a, damage_setting={'γ_2p': np.array([0,
     model_res_list = []
     for γ_2p_i in γ_2p:
         model_paras = (η, δ, θ, πc_o, σ_y, ξ_w, ξ_a, γ_1, γ_2, γ_2p_i, y_bar) 
-        model_res = ode_y(y_grid_long, model_paras, v0=None, ϵ=.5,
+        model_res = ode_y(y_grid_long, model_paras, v0=None, ϵ=1.,
                            tol=1e-8, max_iter=5_000, print_all=False)
         model_res_list.append(model_res)
 
@@ -45,7 +45,7 @@ def solve_value_function(ξ_w, ξ_p, ξ_a, damage_setting={'γ_2p': np.array([0,
     model_paras = (η, δ, θ, πc_o, σ_y, ξ_w, ξ_p, ξ_a, ς, γ_1, γ_2, y_bar, ϕ_i, πd_o)
     model_res = ode_y_jump_approach_one_boundary(y_grid_short, model_paras, 
                                                  v0=np.average(ϕ_i, weights=πd_o, axis=0),
-                                                 ϵ=.5, tol=1e-8, max_iter=5_000, print_all=False)
+                                                 ϵ=1., tol=1e-8, max_iter=5_000, print_all=False)
 
     return model_res_list, model_res
 
@@ -92,7 +92,7 @@ def solve_alternative_ME(ξ_w, ξ_p, ξ_a, baseline_w, baseline_p, baseline_a,
                                                  v0=np.average(ϕ_i, weights=πd_o, axis=0),
                                                  ϵ=.5, tol=1e-8, max_iter=5_000, print_all=False)
 
-    ME_total = δ*η/model_res['e_tilde']
+    ME_total = η/model_res['e_tilde']
 
     # Uncertainty decomposition
     model_paras_new = (η, δ, θ, πc_o, σ_y, ξ_w, ξ_p, ξ_a, γ_1, γ_2, ϕ_i, πd_o)
