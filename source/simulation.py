880c4f8122ac56d99dba0161318568b47cdbc8ea
hanxu <hanxuh@uchicago.edu>
2021-03-30 03:16:06 +0800

    added cases without jump but with ambiguity

--- source/simulation.py
+++ source/simulation.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def simulation(y_grid, e_grid, h_grid, g_grid, θ=1.86/1000., y_start=.5394, T=50):
+def simulation(y_grid, e_grid, h_grid, g_grid, θ=1.86/1000., y_start=.5394, T=100, σ_y=1.2*1.86/1000., baseline=True):
     Et = np.zeros(T+1)
     yt = np.zeros(T+1)
     ht = np.zeros(T+1)
@@ -12,5 +12,8 @@ def simulation(y_grid, e_grid, h_grid, g_grid, θ=1.86/1000., y_start=.5394, T=5
         for n in range(gt.shape[0]):
             gt[n, i] = np.interp(y_start, y_grid, g_grid[n])
         yt[i] = y_start
-        y_start = y_start + Et[i]*θ
+        if baseline:
+            y_start = y_start + Et[i]*θ
+        else:
+            y_start = y_start + Et[i]*(θ+σ_y*ht[i])
     return Et, yt, ht, gt
