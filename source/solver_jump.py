2403d997f0d6795d9ff90d30f80313e5b4c87009
SuriChen1028 <shirui.chan@gmail.com>
2021-03-30 16:42:07 -0500

    change of initial temperature

--- source/solver_jump.py
+++ source/solver_jump.py
@@ -334,3 +334,96 @@ def approach_one_100(y_grid, numy_bar, args, ϵ=0.3, tol=1e-8, max_iter=10_000):
     ϕ_list = np.array(ϕ_list)
     solution = solve_jump_100(y_grid, numy_bar, ϕ_list, args, ϵ, tol, max_iter)
     return solution, ϕ_list
+
+
+def generate_weight(params, ems, dϕdy, y_grid, args_weight=()):
+    δ, η, γ1, γ2, ȳ, σy, ξa = args_weight
+    num, _ = params.shape
+    n_y = len(y_grid)
+    weight =  np.zeros((num, n_y))
+    for i in range(num):
+        θ, γ3 = params[i]
+        dΛ = γ1 + γ2*y_grid + γ3*(y_grid-ȳ)*(y_grid>ȳ)
+        ddΛ = γ2 + γ3*(y_grid>ȳ)
+        temp = dϕdy*ems*θ + (η-1)/δ*dΛ*ems*θ + 1/2*(η-1)/δ*ddΛ*σy**2*ems**2
+        weight[i] = -1/ξa*temp
+    weight = weight - np.max(weight, axis=0)
+    return weight
+
+
+# +
+def solve_smooth_unreveal(y_grid, args, max_iter, tol, ϵ,):
+    """
+    solve for step one, ϕⱼ for individual damage function
+    
+    Parameter
+    ---
+    args: δ, η, θ_list, σy, γ1, γ2, γ3, ȳ, ξa, ξw
+    """
+    δ, η, θ_list, σy, γ1, γ2, γ3_list, ȳ, ξa, ξw = args
+    params = list()
+    for θ_i in θ_list:
+        for γ3 in γ3_list:
+            params.append([θ_i, γ3])
+    params = np.array(params)
+    θ_long = params[:,0]
+    γ3_long = params[:,1]
+    args_weight = (δ, η, γ1, γ2, ȳ, σy, ξa)
+    dy = y_grid[1] - y_grid[0]
+    n_y = len(y_grid)
+#     dΛ = γ1 + γ2*y_grid + γ3*(y_grid-ȳ)*(y_grid>ȳ)
+#     ddΛ = γ2 + γ3*(y_grid>ȳ)
+    ϕ = - η*y_grid**2
+    ems = η
+    ems_old = ems
+    πo = np.ones((len(params), n_y))/len(params)
+    lhs_error = 1
+    episode = 0
+    while lhs_error > tol and episode < max_iter:
+        ϕ_old = ϕ.copy()
+        dϕdy = derivative_1d(ϕ, 1, dy, "up")
+        dϕdyy = derivative_1d(ϕ, 2, dy, "up")
+#         temp1 = dϕdy + (η-1)/δ*dΛ
+#         temp2 = dϕdyy + (de)
+        # update belief
+        weight = generate_weight(params, ems, dϕdy, y_grid, args_weight)
+        π = πo*np.exp(weight)
+        π[π <= 1e-15] = 1e-15
+        π = π/np.sum(π, axis=0)
+        # update control
+        temp = dϕdy + (η-1)/δ*(γ1 + γ2*y_grid)
+        ddΛ = γ2 + γ3_long@π*(y_grid>ȳ)
+        a = (dϕdyy  + (η-1)/δ*ddΛ)*σy**2
+        b = (θ_long@π)*temp + (η-1)/δ*((θ_long*γ3_long)@π)*(y_grid - ȳ)*(y_grid>ȳ)
+        c = η
+        Δ = b**2 - 4*a*c
+        Δ[Δ < 0] = 0
+        root1 = (-b - np.sqrt(Δ))/(2*a)
+        root2 = (-b + np.sqrt(Δ))/(2*a)
+        if root1.all() > 0:
+            ems_new = root1
+        else:
+            ems_new = root2
+        ems_new[ems_new < 1e-15] = 1e-15
+        ems = ems_new*0.5 + ems_old*0.5
+        # solve for ode
+        dmg_drift = θ_long@π*(γ1 + γ2*y_grid)*ems + (θ_long*γ3_long)@π*(y_grid-ȳ)*(y_grid>ȳ)*ems\
+        + 1/2*(γ3_long@π)*(y_grid>ȳ)*σy**2*ems**2 + 1/2*γ2*σy**2*ems**2
+        A = - δ*np.ones(y_grid.shape)
+        B = (θ_long@π)*ems
+#         C = np.zeros(y_grid.shape)
+        C = ems**2*σy**2/2
+        D = η*np.log(ems) + ξa*np.sum(π*(np.log(π) - np.log(πo)), axis=0)\
+            + (η-1)/δ*dmg_drift
+#         - 1/(2*ξw)*temp**2*ems**2*σy**2\
+#         + 1/2*(η-1)/δ*ddΛ*ems**2*σy**2
+        ϕ_new = solve_ode(A, B, C, D, y_grid, ϕ, ϵ, (False, 0))
+        rhs = -δ*ϕ_new + B*dϕdy + C*dϕdyy + D
+        rhs_error = np.max(abs(rhs))
+        lhs_error = np.max(abs((ϕ_new - ϕ_old)/ϵ))
+        ϕ = ϕ_new
+        ems_old = ems
+        episode += 1
+    print("episode: {},\t ode error: {},\t ft error: {}".format(episode, rhs_error, lhs_error))
+    h = - temp*ems*σy/ξw
+    return ϕ, ems, π
