8ba0da464919192990264cda90633b96c20ddfec
SuriChen1028 <shirui.chan@gmail.com>
2021-04-05 01:13:56 -0500

    try plotting packages

---
+++ source/Untitled.py
@@ -0,0 +1,10 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.9.1
+# ---
