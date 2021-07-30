e13db15e0cae4dfe5e29f23a6c243e302f7a298d
zhaozhao99 <danielzhaozhao777@gmail.com>
2021-07-29 22:59:13 -0400

    Delete derivative_computer.m

--- MATLAB/derivative_computer.m
+++
@@ -1,6 +0,0 @@
-function [derivative_array] = derivative_computer(array)
-    array_reform = [0; array(1:end-1,:)];
-    derivative_array_temp = array - array_reform;
-    derivative_array = derivative_array_temp(2:end,:);
-end
-
